409a90fc4362fb498f2334da31dc1f19bb4de839
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2024-01-30 17:42:44 +0000

    video

--- lecture-source/0-diffprog/tex/build.sh
+++ lecture-source/0-diffprog/tex/build.sh
@@ -10,7 +10,9 @@ convert -geometry 1920x1080 -density 320 diffprog.pdf ../diffprog/slides/%04d.pn
 
 latexmk -jobname=diffprog-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' diffprog
 cp diffprog-handouts.pdf ../../../docs/handouts/
+cp diffprog.pdf ../../../docs/lectures/
 git add ../../../docs/handouts/
+git add ../../../docs/lectures/
 
 #if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
 #    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh differentiate.pdf
