62eb132fcc794e6ff9fe4ff1eadb7bd37dd8f3b8
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2025-01-22 14:44:02 +0000

    updates

--- lecture-source/0-diffprog/tex/build.sh
+++ lecture-source/0-diffprog/tex/build.sh
@@ -6,13 +6,19 @@ pushd $SCRIPT_DIR
 
 latexmk -pdf diffprog
 
-convert -geometry 1920x1080 -density 320 diffprog.pdf ../diffprog/slides/%04d.png
+latexmk -jobname=diffprog-slides2per -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' diffprog
+latexmk -jobname=diffprog-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\articlemode{articlemode}\input{%S}"' diffprog
 
-latexmk -jobname=diffprog-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' diffprog
+cp diffprog-slides2per.pdf ../../../docs/slides2per/diffprog-slides2per.pdf
 cp diffprog-handouts.pdf ../../../docs/handouts/
 cp diffprog.pdf ../../../docs/lectures/
+git add ../../../docs/slides2per/
 git add ../../../docs/handouts/
 git add ../../../docs/lectures/
+
+magick convert -geometry 1920x1080 -density 320 diffprog.pdf ../diffprog/slides/%04d.png
+git add ../diffprog/slides/*
+
 
 #if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
 #    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh differentiate.pdf
