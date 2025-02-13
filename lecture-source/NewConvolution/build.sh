1e9263c60834730fcc8972a52e46955adb0f3196
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2025-02-13 21:54:54 +0000

    new conv

--- lecture-source/NewConvolution/build.sh
+++ lecture-source/NewConvolution/build.sh
@@ -1,6 +1,8 @@
 cp -r NewConvolution.pdf ../../docs/lectures/
 git add ../../docs/lectures/NewConvolution.pdf
 
+cp NewConvolution-handouts.pdf ../../docs/slides2per/NewConvolution-slides2per.pdf
+git add ../../docs/slides2per/
+
 cp NewConvolution-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
-
