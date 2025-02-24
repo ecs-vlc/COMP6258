bc24b19c118066709a56d4ff0441c8ecbac583e5
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2025-02-24 22:42:06 +0000

    embeddings

--- lecture-source/15-embeddings/build.sh
+++ lecture-source/15-embeddings/build.sh
@@ -1,5 +1,10 @@
-# cp -r Embeddings ../../docs/lectures/
-# git add ../../docs/lectures/Embeddings
+cp -r Embeddings.pdf ../../docs/lectures/
+git add ../../docs/lectures/Embeddings.pdf
 
 cp Embeddings-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+cp Embeddings-handouts.pdf ../../docs/slides2per/Embeddings-slides2per.pdf
+git add ../../docs/slides2per/
+
+
