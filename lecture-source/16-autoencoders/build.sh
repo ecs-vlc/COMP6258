bdfc73f5b89025c1798777c7d7a58ca6b8aa72f9
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2025-03-06 20:12:03 +0000

    slides

--- lecture-source/16-autoencoders/build.sh
+++ lecture-source/16-autoencoders/build.sh
@@ -2,9 +2,14 @@ latexmk -pdf autoencoders
 cp autoencoders.pdf ../../docs/lectures/
 git add ../../docs/lectures/
 
-latexmk -jobname=autoencoders-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' autoencoders
+latexmk -jobname=autoencoders-slides2per -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' autoencoders
+cp autoencoders-slides2per.pdf ../../docs/slides2per/
+git add ../../docs/slides2per/
+
+latexmk -jobname=autoencoders-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\articlemode{articlemode}\input{%S}"' autoencoders
 cp autoencoders-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
 
 if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
     /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh autoencoders.pdf notwide
